b5611a36a28bcdce0f2c67bee168c4e7ed30264a
omorfiamou <aristotelis@omorfiamou.gr>
2020-11-08 23:04:39 +0200

    Stefanos is fixing the issue

--- Player.cs
+++ Player.cs
@@ -19,15 +19,18 @@ public class Player : Area2D
 		velocity.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
 		velocity.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
 
+		var animatedSprite = GetNode<AnimatedSprite>("PlayerSprite");
+
 		if (velocity.Length() > 0)
 		{
 			velocity = velocity.Normalized() * Speed;
+			animatedSprite.Play();
+		}
+		else
+		{
+			animatedSprite.Stop();
 		}
 		
 		Position += velocity * delta;
-		//Position = new Vector2(
-		//	x: Mathf.Clamp(Position.x, 0, _screenSize.x),
-		//	y: Mathf.Clamp(Position.y, 0, _screenSize.y)
-		//);
 	}
 }
